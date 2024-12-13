1c3ea19598939551ec31a64fdb1898cef5cf5862
Huy Le Ngoc (T) <hlengoc@deloitte.ca>
2024-12-13 10:52:39 +0700

    Initial base project

---
+++ Practical.DuendeIdentityServer/Movies.API/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Hosting;
+using Movies.API.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MoviesContext>(options =>
+        options.UseInMemoryDatabase("Movies"));
+
+builder.Services.AddAuthentication("Bearer")
+        .AddJwtBearer("Bearer", options =>
+        {
+            options.Authority = "https://localhost:5005";
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false
+            };
+        });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient", "movies_mvc_client"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var moviesContext = services.GetRequiredService<MoviesContext>();
+    MoviesContextSeed.SeedAsync(moviesContext);
+}
+
+app.Run();
